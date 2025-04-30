ec8643b558c574eef5868fa51390a836222b834a
lastsaintcrucified <lastsaintcrucified@gmail.com>
2025-04-30 09:04:11 +0600

    sign up fixed

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -6,6 +6,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # 👈 This hashes the password
+        user.save()
+        return user
 
 class ProjectSerializer(serializers.ModelSerializer):
     assigned_users = serializers.PrimaryKeyRelatedField(
