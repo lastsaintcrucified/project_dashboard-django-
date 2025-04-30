136383e00c83194aa6c44aa6e26c1891b17dba0a
lastsaintcrucified <lastsaintcrucified@gmail.com>
2025-04-30 09:12:03 +0600

    sign up fixed again

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'role', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
         }
